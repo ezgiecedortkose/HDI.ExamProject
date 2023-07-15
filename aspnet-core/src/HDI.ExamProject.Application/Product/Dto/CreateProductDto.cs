c61c8e80c979d74be53824a1bf87e9ad99590fab
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:09:33 +0300

    Product Crud Dto added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Product/Dto/CreateProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Product.Dto
+{
+    public class CreateProductDto : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public decimal RiskCost { get; set; }
+    }
+}
+
