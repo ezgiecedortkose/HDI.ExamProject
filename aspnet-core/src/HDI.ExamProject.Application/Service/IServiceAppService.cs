9faa9570038bf8baf5360fd504e119528c491319
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:15:12 +0300

    Service App Service added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Service/IServiceAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Abp.Application.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using HDI.ExamProject.Dealer.Dto;
+using HDI.ExamProject.Service.Dto;
+
+namespace HDI.ExamProject.Service
+{
+    public interface IServiceAppService : IAsyncCrudAppService<ServiceDto, Guid, PagedAndSortedResultRequestDto, CreateServiceDto, UpdateServiceDto>
+    {
+    }
+
+}
+
---
+++ aspnet-core/src/HDI.ExamProject.Application/Service/ServiceAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using AutoMapper.Internal.Mappers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Abp.Application.Services.Dto;
+using HDI.ExamProject.Entities;
+using HDI.ExamProject.Service.Dto;
+
+namespace HDI.ExamProject.Service
+{
+    public class ServiceAppService : AsyncCrudAppService<Services, ServiceDto, Guid, PagedAndSortedResultRequestDto, CreateServiceDto, UpdateServiceDto>, IServiceAppService
+    {
+        public ServiceAppService(IRepository<Services, Guid> repository)
+            : base(repository)
+        {
+        }
+    }
+
+}
