269a9d91a314545ed1f502a2ab3160fb9b21c110
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:13:49 +0300

    Dealers entities added

---
+++ aspnet-core/src/HDI.ExamProject.Core/Entities/Dealers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Castle.Core;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Entities
+{
+	public class Dealers : Entity<Guid>
+	{
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public long ExchangeCount { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+
+        public Dealers()
+        {
+            Products = new List<Products>();
+        }
+    }
+}
+
