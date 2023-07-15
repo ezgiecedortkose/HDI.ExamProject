e6e12f0a0327638e9be3302b39d22e66c8e78c20
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:07:32 +0300

    Products entities added

---
+++ aspnet-core/src/HDI.ExamProject.Core/Entities/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Entities
+{
+	public class Products : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public decimal RiskCost { get; set; }
+
+        public long RepairCount { get; set; }
+        public long ExchangeCount { get; set; }
+        public decimal CostPrice { get; set; }
+
+        public ICollection<Customers> Customers { get; set; }
+        public ICollection<Dealers> Dealers { get; set; }
+        public ICollection<Services> Services { get; set; }
+
+        public Products()
+        {
+            Customers = new List<Customers>();
+            Dealers = new List<Dealers>();
+            Services = new List<Services>();
+        }
+    }
+}
+
