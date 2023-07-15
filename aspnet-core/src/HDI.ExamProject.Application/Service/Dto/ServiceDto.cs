b1907db5dced92905337e370bd25854273d0761c
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:15:01 +0300

    Service crud dto added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Service/Dto/ServiceDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Entities;
+using HDI.ExamProject.Product.Dto;
+
+namespace HDI.ExamProject.Service.Dto
+{
+	public class ServiceDto : IEntityDto<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<ProductDto> Products { get; set; }
+    }
+}
+
---
+++ aspnet-core/src/HDI.ExamProject.Application/Service/Dto/UpdateServiceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Service.Dto
+{
+	public class UpdateServiceDto : IEntityDto<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
+
