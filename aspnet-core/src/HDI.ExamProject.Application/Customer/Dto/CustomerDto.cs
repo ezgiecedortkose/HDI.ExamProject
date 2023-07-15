cb8541c942321c0f10044dce05c7f8f9a3eafa62
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:11:41 +0300

    Customer crud dto added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Customer/Dto/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+using HDI.ExamProject.Product.Dto;
+
+namespace HDI.ExamProject.Customer.Dto
+{
+    public class CustomerDto : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CustomerRegistrationDate { get; set; }
+        public List<ProductDto> CustomerProductsPurchased { get; set; }
+
+        public ICollection<ProductDto> Products { get; set; }
+
+    }
+}
+
---
+++ aspnet-core/src/HDI.ExamProject.Application/Customer/Dto/UpdateCustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Customer.Dto
+{
+    public class UpdateCustomerDto : Entity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; } 
+    }
+}
+
