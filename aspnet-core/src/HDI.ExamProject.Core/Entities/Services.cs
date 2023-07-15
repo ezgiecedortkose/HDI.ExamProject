959b163f713f5fb7250726f53c7525533e9ee8ad
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:14:50 +0300

    Services entities added

---
+++ aspnet-core/src/HDI.ExamProject.Core/Entities/Services.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Entities
+{
+	public class Services : Entity<Guid>
+	{
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+
+        public Services()
+        {
+            Products = new List<Products>();
+        }
+    }
+}
+
