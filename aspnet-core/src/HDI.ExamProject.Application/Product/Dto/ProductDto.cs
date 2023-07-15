d4954c884dd8eca02feaf94632b8b3bac78525c3
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:08:13 +0300

    ProductDto added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Product/Dto/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+using HDI.ExamProject.Customer.Dto;
+using HDI.ExamProject.Dealer.Dto;
+using HDI.ExamProject.Service.Dto;
+
+namespace HDI.ExamProject.Product.Dto
+{
+    public class ProductDto : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public decimal RiskCost { get; set; }
+
+        public ICollection<CustomerDto> Customers { get; set; }
+        public ICollection<DealerDto> Dealers { get; set; }
+        public ICollection<ServiceDto> Services { get; set; }
+    }
+
+}
+
