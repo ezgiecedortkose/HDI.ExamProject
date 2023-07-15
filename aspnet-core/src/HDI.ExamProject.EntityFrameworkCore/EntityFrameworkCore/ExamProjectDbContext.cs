7883dcdc77df2eba862d2a12d0b8e14effbd129c
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:18:49 +0300

    DbContext created

--- aspnet-core/src/HDI.ExamProject.EntityFrameworkCore/EntityFrameworkCore/ExamProjectDbContext.cs
+++ aspnet-core/src/HDI.ExamProject.EntityFrameworkCore/EntityFrameworkCore/ExamProjectDbContext.cs
@@ -3,13 +3,19 @@ using Abp.Zero.EntityFrameworkCore;
 using HDI.ExamProject.Authorization.Roles;
 using HDI.ExamProject.Authorization.Users;
 using HDI.ExamProject.MultiTenancy;
+using HDI.ExamProject.Entities;
 
 namespace HDI.ExamProject.EntityFrameworkCore
 {
     public class ExamProjectDbContext : AbpZeroDbContext<Tenant, Role, User, ExamProjectDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Dealers> Dealers { get; set; }
+        public DbSet<Services> Services { get; set; }
+
         public ExamProjectDbContext(DbContextOptions<ExamProjectDbContext> options)
             : base(options)
         {
