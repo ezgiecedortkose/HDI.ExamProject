36d0b4b3336ff7148cb4cf89216dec2e1bd1cbcd
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:17:55 +0300

    created sales controller

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Controllers/SalesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HDI.ExamProject.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HDI.ExamProject.Web.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly ICustomerAppService _customerAppService;
+
+        public SalesController(ICustomerAppService customerAppService)
+        {
+            _customerAppService = customerAppService;
+        }
+
+        public IActionResult Sales(Guid dealerId)
+        {
+            var customers = _customerAppService.GetCustomersByDealerId(dealerId);
+
+            return View(customers);
+        }
+    }
+
+}
+
