6fc9725e2b13e0376a1b9f9e70a534c69406ba68
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:11:06 +0300

    Customers entities added

---
+++ aspnet-core/src/HDI.ExamProject.Core/Entities/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Abp.Domain.Entities;
+
+namespace HDI.ExamProject.Entities
+{
+    public class Customers : Entity<Guid>
+    {
+        public Guid DealerId;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+
+        public Customers()
+        {
+            Products = new List<Products>();
+        }
+    }
+}
+
