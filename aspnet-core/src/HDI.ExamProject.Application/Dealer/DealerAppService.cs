d363a368f96bf6b17935ae389a06b3c073d128b2
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-16 02:14:16 +0300

    dealer service added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Dealer/DealerAppService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Abp.Application.Services.Dto;
+using HDI.ExamProject.Dealer.Dto;
+using HDI.ExamProject.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HDI.ExamProject.Dealer
+{
+    public class DealerAppService : AsyncCrudAppService<Dealers, DealerDto, Guid, PagedAndSortedResultRequestDto, CreateDealerDto, UpdateDealerDto>, IDealerAppService
+    {
+        public DealerAppService(IRepository<Dealers, Guid> repository)
+            : base(repository)
+        {
+        }
+
+        public async Task<ListResultDto<DealerDto>> GetAll()
+        {
+            var dealers = await Repository.GetAllListAsync();
+            return new ListResultDto<DealerDto>(ObjectMapper.Map<List<DealerDto>>(dealers));
+        }
+    }
+}
+
+
---
+++ aspnet-core/src/HDI.ExamProject.Application/Dealer/IDealerAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using HDI.ExamProject.Dealer.Dto;
+
+namespace HDI.ExamProject.Dealer
+{
+    public interface IDealerAppService : IAsyncCrudAppService<DealerDto, Guid, PagedAndSortedResultRequestDto, CreateDealerDto, UpdateDealerDto>
+    {
+        Task<ListResultDto<DealerDto>> GetAll();
+    }
+}
+
