24a3788cff503e178dbf12463f70836c139d2a37
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 17:20:05 +0300

    CustomerViewModel added

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Models/CustomerView/CustomerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HDI.ExamProject.Web.Models.ProductView;
+using System.Collections.Generic;
+
+namespace HDI.ExamProject.Web.Models.CustomerView
+{
+    public class CustomerViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CustomerRegistrationDate { get; set; }
+        public List<ProductViewModel> CustomerProductsPurchased { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+
+}
+
