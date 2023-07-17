891a1ece0023c9dd9ce0184d8e92c568c7da1f49
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 16:08:42 +0300

    DashboardDataViewModel added

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Models/DashboardDataView/DashboardDataViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HDI.ExamProject.DashboardData.Dto;
+using System.Collections.Generic;
+
+namespace HDI.ExamProject.Web.Models.DashboardDataView
+{
+    public class DashboardDataViewModel
+    {
+        public List<ProductSalesViewModel> ProductSales { get; set; }
+        public List<ServiceCenterRepairsViewModel> ServiceCenterRepairs { get; set; }
+    }
+
+    public class ProductSalesViewModel
+    {
+        public string ProductName { get; set; }
+        public int SalesCount { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+
+    public class ServiceCenterRepairsViewModel
+    {
+        public string ServiceCenterName { get; set; }
+        public int RepairCount { get; set; }
+    }
+
+}
+
