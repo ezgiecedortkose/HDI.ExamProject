1c59f2f49bca3d190464ca0ada8b9433a7e9e279
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 17:20:35 +0300

    ServiceViewModel added

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Models/ServiceView/ServiceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HDI.ExamProject.Web.Models.ProductView;
+using System.Collections.Generic;
+
+namespace HDI.ExamProject.Web.Models.ServiceView
+{
+    public class ServiceViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+
+}
+
