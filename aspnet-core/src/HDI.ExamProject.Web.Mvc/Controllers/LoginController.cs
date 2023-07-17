4a7db0598948cba9dee8931ad944fe86235b6106
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 16:07:53 +0300

    LoginController fixed

--- aspnet-core/src/HDI.ExamProject.Web.Mvc/Controllers/LoginController.cs
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Controllers/LoginController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using HDI.ExamProject.Dealer;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HDI.ExamProject.Web.Controllers
 {
     public class LoginController : Controller
@@ -20,25 +18,18 @@ namespace HDI.ExamProject.Web.Controllers
 
         public IActionResult Index()
         {
-            var dealers = _dealerAppService.GetAll(); // Tüm bayileri getir
-
-            // Login sayfasına dealers'ı ve diğer gerekli verileri gönder
+            var dealers = _dealerAppService.GetAll();
             return View(dealers);
         }
 
         [HttpPost]
         public IActionResult Login(string username, string password, int dealerId)
         {
-            // Kullanıcı adı, şifre ve bayi seçimini kontrol et
             if (username == "admin" && password == "password")
             {
-                // Doğrulama işlemlerini yap ve giriş işlemini gerçekleştir
-
-                // Başarılı giriş durumunda, bayiye özel satış paneline yönlendir
                 return RedirectToAction("Sales", "Sales", new { dealerId = dealerId });
             }
 
-            // Giriş başarısız olduğunda, hata mesajı göster veya tekrar login sayfasına yönlendir
             return View("Index");
         }
     }
