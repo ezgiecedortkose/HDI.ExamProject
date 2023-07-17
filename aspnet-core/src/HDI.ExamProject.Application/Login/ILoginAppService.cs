68f3fe1e769ba2d00baa0ef02aef800a561ea839
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 16:06:30 +0300

    LoginAppService and interface added

---
+++ aspnet-core/src/HDI.ExamProject.Application/Login/ILoginAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HDI.ExamProject.Login
+{
+    public interface ILoginAppService
+    {
+        Task<bool> LoginAsync(string username, string password);
+    }
+
+}
+
---
+++ aspnet-core/src/HDI.ExamProject.Application/Login/LoginAppService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace HDI.ExamProject.Login
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class LoginAppService : ILoginAppService
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public LoginAppService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+            {
+                return false;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, password, false, lockoutOnFailure: false);
+            if (!result.Succeeded)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
+
