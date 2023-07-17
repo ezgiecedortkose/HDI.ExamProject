ecda060b4eae645806d4631fdaae7faf6392b75f
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 16:06:48 +0300

    LoginViewModel added

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Models/LoginView/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HDI.ExamProject.Web.Models.LoginView
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı gereklidir.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
+
