2e96d7755e2cc1f1821da5c435e0ee5454b4a5e3
ezgiecedortkose <65659491+ezgiecedortkose@users.noreply.github.com>
2023-07-17 17:20:16 +0300

    DealerViewModel added

---
+++ aspnet-core/src/HDI.ExamProject.Web.Mvc/Models/DealerView/DealerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using HDI.ExamProject.Web.Models.ProductView;
+
+namespace HDI.ExamProject.Web.Models.DealerView
+{
+    public class DealerViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+
+}
+
